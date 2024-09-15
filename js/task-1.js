d3f95d00706db0f4afbc34a777e400f2de52cf64
Michal Figiel <50012494+micfig76@users.noreply.github.com>
2024-09-15 16:53:44 +0200

    moved files to folder

--- js/task-1.js
+++ js/task-1.js
@@ -6,3 +6,15 @@ document.querySelectorAll("li.item").forEach(li => {
     );
     
 });
+
+// // Count the number of <li> elements with the class "item"
+// const itemCount = document.querySelectorAll("li.item").length;
+// console.log(`Total items: ${itemCount}`);
+
+// // Iterate over each <li> element with the class "item"
+// document.querySelectorAll("li.item").forEach(li => {
+//     console.log(
+//         li.querySelector("h2").textContent, // Log the text content of the <h2> tag
+//         li.querySelectorAll("li").length    // Log the number of nested <li> elements
+//     );
+// });
