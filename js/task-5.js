a0546c69f6a0fa2d773f0625e1f3e029539936b4
Michal Figiel <50012494+micfig76@users.noreply.github.com>
2024-09-18 22:41:51 +0200

    added code to task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
+const btn = document.getElementById('change');
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+});
