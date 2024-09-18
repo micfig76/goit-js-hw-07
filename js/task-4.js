48b3abb3cf293a83754f02dc036709980418c522
Michal Figiel <50012494+micfig76@users.noreply.github.com>
2024-09-18 19:43:45 +0200

    added code to task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const emailInput = loginForm.querySelector('[name="email"]'); 
+
+  const passwordInput = loginForm.querySelector('[name="password"]');
+
+  const formData = {
+    email: emailInput.value.trim(),
+    password: passwordInput.value.trim()
+  };
+
+  if (!formData.email || !formData.password) {
+    alert('All form fields must be filled.');
+    return;
+  }
+
+  console.log('Form data:', formData);
+
+  return formData;
+
+loginForm.reset();
+});
