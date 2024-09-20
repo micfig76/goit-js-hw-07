61d1a61884b9d80817cdc906859ac61aae4c8331
Michal Figiel <50012494+micfig76@users.noreply.github.com>
2024-09-20 18:53:09 +0200

    added code to task 6

--- js/task-6.js
+++ js/task-6.js
@@ -4,9 +4,49 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-btn.addEventListener("click", () => {
-  document.body.style.backgroundColor = getRandomHexColor();
+const boxes = document.querySelector("div#boxes");
 
- })
+const destroyBoxes = () => {
+  [...boxes.children].forEach(child => child.remove());
 
- btn.textContent = "change color"
+};
+
+const createBoxes = (amount) => {
+  destroyBoxes();
+  console.log(amount);
+
+  const BASE_SIZE = 30;
+  const SIZE_RATIO = 10;
+
+  const freshBoxes = Array.from({ length: amount }, (_, i) => {
+    const size = `${BASE_SIZE + (i + boxes.children.length) * SIZE_RATIO}px`;
+
+    const box = document.createElement("div");
+    box.style.width = size; 
+    box.style.height = size;
+    box.style.backgroundColor = getRandomHexColor();
+
+    return box;
+});
+
+const fragment = document.createDocumentFragment();
+fragment.append(...freshBoxes);
+
+boxes.prepend(fragment);
+
+};
+
+document.querySelector("button[data-create]").addEventListener("click", () => {
+  const input = document.querySelector("input");
+  const amount = parseInt(input.valueAsNumber, 10);
+
+  if (amount < 1 || amount > 100) {
+    alert("Amount must be between 1 and 100.");
+    return;
+  }
+
+  createBoxes(amount);
+  input.value = "";
+});
+
+document.querySelector("button[data-destroy]").addEventListener("click", destroyBoxes);
